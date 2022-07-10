3550a3c3aea5cb6e8b4bad25af6d344e8ed101d0
Paulo <fernandes.josepaulo95@gmail.com>
2022-07-10 01:06:01 -0300

    refactor test naming

--- src/__tests__/hello.test.js
+++ src/__tests__/hello.test.js
@@ -2,8 +2,8 @@ import { assert, describe, expect, it } from 'vitest'
 import { deduceState, PLAYER_UNIT as P, ENEMY_UNIT as E } from '../services/GameState'
 
 
-describe('gamestate', () => {
-  it('recognizes empty board', () => {
+describe('recognizes gamestate', () => {
+  it('empty board', () => {
     const empty_board = [[0,0], [0,0]]
     expect(deduceState(empty_board)).toEqual("empty")
 
@@ -11,7 +11,7 @@ describe('gamestate', () => {
     expect(deduceState(empty_board_1)).toEqual("empty")
   })
 
-  it('recognizes player victory', () => {
+  it('player victory', () => {
     const only_player_board = [[0,P], [0,0]]
     expect(deduceState(only_player_board)).toEqual("player_victory")
 
@@ -19,7 +19,7 @@ describe('gamestate', () => {
     expect(deduceState(only_player_board_1)).toEqual("player_victory")
   })
 
-  it('recognizes enemy victory', () => {
+  it('enemy victory', () => {
     const only_player_board = [[0,E], [0,0]]
     expect(deduceState(only_player_board)).toEqual("enemy_victory")
 
@@ -27,7 +27,7 @@ describe('gamestate', () => {
     expect(deduceState(only_player_board_1)).toEqual("enemy_victory")
   })
 
-  it('recognizes disputing', () => {
+  it('disputing', () => {
     const only_player_board = [[0,E], [P,1]]
     expect(deduceState(only_player_board)).toEqual("disputing")
 
