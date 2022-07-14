31267791f33dab907f289f351d980df99ccf7a97
Paulo <fernandes.josepaulo95@gmail.com>
2022-07-13 21:31:43 -0300

    wip add component board

---
+++ src/components/Board.vue
@@ -0,0 +1,18 @@
+  <template>
+      <h1>{{ msg }}</h1>
+  </template>
+
+  <script lang="ts">
+  import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    name: 'Board',
+    props: {
+      msg: String,
+    },
+  });
+  </script>
+
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped>
+  </style>
