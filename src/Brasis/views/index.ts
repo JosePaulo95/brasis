e65544509483ee4a9d453673436923b488054f42
Paulo <fernandes.josepaulo95@gmail.com>
2022-07-14 19:51:21 -0300

    getBgSprite using assets folder

--- src/Brasis/views/index.ts
+++ src/Brasis/views/index.ts
@@ -1,5 +1,6 @@
 
 
-export function getBackgroundSprite (sprite_code: String) {
-    return require('@/assets/logo.png')
+export function getBackgroundSprite (sprite_code: number) {
+    const code = String(sprite_code).padStart(3, "0")
+    return require('@/assets/Tilesets/terrain arido/tile'+code+'.png')
 }
