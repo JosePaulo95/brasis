276ed5ea3fc320646e5d7ddca1e8f077258c9b87
Paulo <fernandes.josepaulo95@gmail.com>
2022-07-16 00:42:39 -0300

    fix initialize

--- src/Brasis/models/board.ts
+++ src/Brasis/models/board.ts
@@ -14,25 +14,26 @@ export default class BoardModel{
             [0,0,0],
             [0,0,0],
             [0,0,0],
-        ].map(row => row.map(cell => new BaseLayerModel()))
+        ].map(row => row.map(cell => new BaseLayerModel(cell)))
         
         this.bg_board = [
             [55,55,55],
             [55,55,55],
             [55,55,55],
-        ].map(row => row.map(cell => new BgLayerModel()))
+        ].map(row => row.map(cell => new BgLayerModel(cell)))
         
         this.actors_board = [
             [0,0,0],
             [0,1,0],
             [0,0,0],
-        ].map(row => row.map(cell => new ActorLayerModel()))
+        ].map(row => row.map(cell => new ActorLayerModel(cell)))
 
         this.hud_board = [
             [0,0,0],
             [0,0,0],
             [0,0,0],
-        ].map(row => row.map(cell => new HUDLayerModel()))
+        ].map(row => row.map(cell => new HUDLayerModel(cell)))
+        
     }
     propagateSelection({ old_x, old_y, x, y}:{ old_x:number, old_y:number, x:number, y:number}): BoardModel {
         //MOVE current hud select
@@ -44,19 +45,19 @@ export default class BoardModel{
 
         const prev_selected = old_x!=undefined?this.hud_board[old_x][old_y]:null
         const prev_actor = old_x!=undefined?this.actors_board[old_x][old_y]:null
-        const current_empty = this.actors_board[x][y]==0
+        const current_empty = this.actors_board[x][y].value==0
 
-        old_x!=undefined&&(this.hud_board[old_x][old_y] = 0)
+        old_x!=undefined&&(this.hud_board[old_x][old_y].value = 0)
 
         if(prev_actor && current_empty){
             if(prev_selected){
-                this.actors_board[old_x][old_y] = 0
-                this.actors_board[x][y] = 1
+                this.actors_board[old_x][old_y].value = 0
+                this.actors_board[x][y].value = 1
             }else{
-                this.hud_board[x][y] = 1
+                this.hud_board[x][y].value = 1
             }
         }else{
-            this.hud_board[x][y] = 1
+            this.hud_board[x][y].value = 1
         }
 
         return this
