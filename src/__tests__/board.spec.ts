aec79f8c20d22da30c259f49405eaf4dfc4bf5b1
Paulo <fernandes.josepaulo95@gmail.com>
2022-07-15 22:54:31 -0300

    [test] selects cells

--- src/__tests__/board.spec.ts
+++ src/__tests__/board.spec.ts
@@ -1,28 +1,20 @@
+import BoardController from '@/Brasis/controllers/board'
+import BoardModel from '@/Brasis/models/board'
 import Board from '@/components/Board.vue'
 
 describe('board component', () => {
-  it('selects character', async () => {
-    // controller seleciona ator em 1,1
-      // [] ele fica selecionado visualmente
-      // [] grid possiveis moves
-      // [] toca um som
-      // [] tem um vfx
+  it('selects cells', async () => {
+    const model = new BoardModel()
+    const controller = new BoardController(model);
+    let state = {}
 
+    expect(model.hud_board[0][0]).toEqual(0)
 
-
-    // ator sabe que fora selecionado e retorna 
-    // model de interação sabe que ele esta selecionado
-
-    // controller selecionar espaço em 1,2
-    // model move ator em (1,1) para (1,2)
-
-    // controller selecionar ator em 1,1
-      // quem selecionou
-      // de quem é a vez
-      // quem foi selecionado
-    // model seleciona ator em 1,1
-    // controller selecionar ator em 1,0
-    // controller selecionar espaço em 1,2
-    // model move ator em (1,0) para (1,2)
+    state = controller.select(0,0)
+    expect(model.hud_board[0][0]).toEqual(1)
+    
+    state = controller.select(1,0)
+    expect(model.hud_board[0][0]).toEqual(0)
+    expect(model.hud_board[1][0]).toEqual(1)
   })
 })
