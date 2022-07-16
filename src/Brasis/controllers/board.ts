d9df9750c9aa81727221736cd63483bdff57bbee
Paulo <fernandes.josepaulo95@gmail.com>
2022-07-15 21:38:39 -0300

    add basic movement

--- src/Brasis/controllers/board.ts
+++ src/Brasis/controllers/board.ts
@@ -2,11 +2,17 @@ import BoardModel from "../models/board";
 
 export default class BoardController{
     model = {} as BoardModel
+    old_x!: number;
+    old_y!: number;
 
     constructor(model: BoardModel){
         this.model = model
     }
     select(x: number, y: number): BoardModel {
+        this.model.propagateSelection({old_x: this.old_x, old_y: this.old_y, x, y})
+        this.old_x = x
+        this.old_y = y
+        
         return this.model
     }
 }
--- src/Brasis/models/board.ts
+++ src/Brasis/models/board.ts
@@ -3,24 +3,36 @@ export default class BoardModel{
     default_board: Array<Array<number>>;
     bg_board: Array<Array<number>>;
     actors_board: Array<Array<number>>;
-
+    
     constructor(level_code="dev"){
         this.default_board = [
             [0,0,0],
             [0,0,0],
             [0,0,0],
         ]
-
+        
         this.bg_board = [
             [55,55,55],
             [55,55,55],
             [55,55,55],
         ]
-
+        
         this.actors_board = [
             [0,0,0],
             [0,1,0],
             [0,0,0],
         ]
+    }
+    propagateSelection({ old_x, old_y, x, y}:{ old_x:number, old_y:number, x:number, y:number}): BoardModel {
+        //se old é um ator e current é espaço vazio
+        //mover
+        const prev_actor = old_x!=undefined?this.actors_board[old_x][old_y]:null
+        const current_empty = this.actors_board[x][y]==0
+        
+        if(prev_actor && current_empty){
+            this.actors_board[old_x][old_y] = 0
+            this.actors_board[x][y] = 1
+        }
+        return this
     }
 }
