37aecc2d8954ee7f9e1b9c9b9e98484bfd39a3af
Paulo <fernandes.josepaulo95@gmail.com>
2022-07-17 18:50:29 -0300

    [wip] add interactions system

--- src/Brasis/controllers/board.ts
+++ src/Brasis/controllers/board.ts
@@ -4,19 +4,41 @@ export default class BoardController{
     model = {} as BoardModel
     old_x!: number;
     old_y!: number;
-
+    
     constructor(model: BoardModel){
         this.model = model
     }
     select(x: number, y: number): BoardModel {
 
         this.manageActorSelection(x,y)
+        this.manageMove(x,y,this.old_x, this.old_y)
 
-        this.model.propagateSelection({old_x: this.old_x, old_y: this.old_y, x, y})
+        // this.model.propagateSelection({old_x: this.old_x, old_y: this.old_y, x, y})
         this.old_x = x
         this.old_y = y
         
         return this.model
+    }
+
+    triggersInteraction(action_code: string, x: number, y: number) {
+        this.manageActorSelection(x,y)
+    }
+
+    manageMove(x: number, y: number, old_x: number, old_y: number) {
+        const prev_actor = this.hasActor(old_x, old_y)
+        const cur_hud = this.getHUD(x, y)
+        if(prev_actor?.value){
+            if(cur_hud.value){
+
+            }else{
+                const possible_houses_1 = this.getNeighbors(old_x, old_y, 1)
+
+                for (let i = 0; i < possible_houses_1.length; i++) {
+                    const house = possible_houses_1[i];
+                    this.model.hud_board[house.x][house.y].value = 0
+                }
+            }
+        }
     }
     manageActorSelection(x: number, y: number) {
         if(this.hasActor(x,y)){
@@ -30,6 +52,9 @@ export default class BoardController{
     }
     hasActor(x: number, y: number) {
         return this.model.actors_board[x] && this.model.actors_board[x][y]
+    }
+    getHUD(x: number, y: number) {
+        return this.model.hud_board[x] && this.model.hud_board[x][y]
     }
     getNeighbors(x: number, y: number, d=1): any {
         if(!this.is_valid_house(x,y)){
--- src/__tests__/board.spec.ts
+++ src/__tests__/board.spec.ts
@@ -35,11 +35,33 @@ describe('board component', () => {
     expect(model.hud_board[1][1].value).toEqual(1)
   })
 
-  it('shows possible moves', async () => {
+  it('neighbors level adjacents', async () => {
     const model = new BoardModel("dev1")
     const controller = new BoardController(model);
-    const neighbors = controller.getNeighbors(1,1,2)
+    const neighbors = controller.getNeighbors(1,1,1)
     console.log(neighbors);
     expect(neighbors.length).toEqual(4)
+  })
+
+  it('action possibleMoves', async () => {
+    const model = new BoardModel("3x3 actor in center")
+    const controller = new BoardController(model);
+
+    controller.triggersInteraction("showPossibleMoves", 1, 1)
+    expect(model.hud_board[0][0].value).toEqual(0)
+    expect(model.hud_board[1][1].value).toEqual(0)
+
+    expect(model.hud_board[1][0].value).toEqual(1)
+    expect(model.hud_board[0][1].value).toEqual(1)
+    expect(model.hud_board[1][2].value).toEqual(1)
+    expect(model.hud_board[2][1].value).toEqual(1)
+
+    //prev_act  cur
+    //v         hud   action(move)
+    //v         act   dismiss, possibleMoves
+    //v         bg    dismiss
+    //f         hud   x
+    //f         act   possibleMoves
+    //f         bg    -
   })
 })
