2c81389bd5e536b5e2df2d17b64b92be392ba751
Paulo <fernandes.josepaulo95@gmail.com>
2022-07-17 11:28:27 -0300

    add level template 4x4

--- src/Brasis/models/board.ts
+++ src/Brasis/models/board.ts
@@ -9,30 +9,64 @@ export default class BoardModel{
     actors_board: Array<Array<ActorLayerModel>>;
     hud_board: Array<Array<HUDLayerModel>>;
 
-    constructor(level_code="dev"){
-        this.default_board = [
-            [0,0,0],
-            [0,0,0],
-            [0,0,0],
-        ].map(row => row.map(cell => new BaseLayerModel(cell)))
+    constructor(level_code?:string){
+        switch (level_code) {
+            case "dev1":
+                this.default_board = [
+                    [0,0,0,0],
+                    [0,0,0,0],
+                    [0,0,0,0],
+                    [0,0,0,0]
+                ].map(row => row.map(cell => new BaseLayerModel(cell)))
+                
+                this.bg_board = [
+                    [55,55,55,55],
+                    [55,55,55,55],
+                    [55,55,55,55],
+                    [55,55,55,55]
+                ].map(row => row.map(cell => new BgLayerModel(cell)))
+                
+                this.actors_board = [
+                    [0,0,0,0],
+                    [0,1,0,0],
+                    [0,0,0,0],
+                    [0,0,0,0],
+                ].map(row => row.map(cell => new ActorLayerModel(cell)))
         
-        this.bg_board = [
-            [55,55,55],
-            [55,55,55],
-            [55,55,55],
-        ].map(row => row.map(cell => new BgLayerModel(cell)))
+                this.hud_board = [
+                    [0,0,0,0],
+                    [0,0,0,0],
+                    [0,0,0,0],
+                    [0,0,0,0],
+                ].map(row => row.map(cell => new HUDLayerModel(cell)))
+                break;
+            default:
+                this.default_board = [
+                    [0,0,0],
+                    [0,0,0],
+                    [0,0,0],
+                ].map(row => row.map(cell => new BaseLayerModel(cell)))
+                
+                this.bg_board = [
+                    [55,55,55],
+                    [55,55,55],
+                    [55,55,55],
+                ].map(row => row.map(cell => new BgLayerModel(cell)))
+                
+                this.actors_board = [
+                    [0,0,0],
+                    [0,1,0],
+                    [0,0,0],
+                ].map(row => row.map(cell => new ActorLayerModel(cell)))
         
-        this.actors_board = [
-            [0,0,0],
-            [0,1,0],
-            [0,0,0],
-        ].map(row => row.map(cell => new ActorLayerModel(cell)))
-
-        this.hud_board = [
-            [0,0,0],
-            [0,0,0],
-            [0,0,0],
-        ].map(row => row.map(cell => new HUDLayerModel(cell)))
+                this.hud_board = [
+                    [0,0,0],
+                    [0,0,0],
+                    [0,0,0],
+                ].map(row => row.map(cell => new HUDLayerModel(cell)))        
+                break;
+        }
+        
         
     }
     propagateSelection({ old_x, old_y, x, y}:{ old_x:number, old_y:number, x:number, y:number}): BoardModel {
