d400fa0337029342adf46c6a86c27fe6705977b8
Paulo <fernandes.josepaulo95@gmail.com>
2022-07-18 17:04:29 -0300

    add matchers *>str

--- src/Brasis/models/InteractionModel.ts
+++ src/Brasis/models/InteractionModel.ts
@@ -7,4 +7,17 @@ export class InteractionModel {
         this.event_key = event_key
         this.method = method
     }
+
+    match(event_key: string) {
+        if(this.event_key == "*"){
+            return true
+        }
+
+        event_key = event_key.includes(">")?event_key:">"+event_key
+
+        const t = this.event_key.split(">")
+        const e = event_key.split(">")
+
+        return (t[0]==e[0] || t[0]=="*") && (t[1]==e[1] || t[1]=="*")
+    }
 } 
---
+++ src/__tests__/interaction.spec.ts
@@ -0,0 +1,20 @@
+import { InteractionModel } from "@/Brasis/models/InteractionModel";
+
+describe('board component', () => {
+    it('eventMethod maps correctly', async () => {
+        const trigger_all = new InteractionModel("*", ()=>{})
+        const trigger_possible_moves = new InteractionModel("*>actor", ()=>{})
+        const trigger_dismiss = new InteractionModel("actor>*", ()=>{})
+
+        expect(trigger_all.match("actor>actor")).toBe(true)
+        expect(trigger_all.match("hud")).toBe(true)
+
+        expect(trigger_possible_moves.match("actor>actor")).toBe(true)
+        expect(trigger_possible_moves.match("actor")).toBe(true)
+        expect(trigger_possible_moves.match("actor>bg")).toBe(false)
+
+        expect(trigger_dismiss.match("actor")).toBe(false)
+        expect(trigger_dismiss.match("hud>actor")).toBe(false)
+        expect(trigger_dismiss.match("actor>bg")).toBe(true)
+    })
+})
