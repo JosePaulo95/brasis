8eeebcecb58f8f08e781b9b4b154ab837060b9f2
Paulo <fernandes.josepaulo95@gmail.com>
2022-07-24 16:05:13 -0300

    deletes commented code

--- src/components/BoardLayers/LayerActors.vue
+++ src/components/BoardLayers/LayerActors.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="cell.value" :class="'actor cell-content'" >
+  <div :class="'actor cell-content'" >
     
   </div>
 </template>
@@ -27,20 +27,11 @@ import { defineComponent } from 'vue';
 <style scoped>
   .actor {
     --i:0;
-    --j:2;
+    --j:0;
     background-image: url("../../assets/Knight/SpriteSheet.png");
     background-size: calc(100%*4);
     background-position: left calc(var(--i)*100%) top calc(var(--j)*100%);
     z-index: 3; 
-    /* --n:.5;
-    --i:3; 
-    --j:0; 
-
-    background-image: url("../../assets/Knight/SeparateAnim/Idle.png");
-    height:calc(16px / var(--n));
-    width:calc(16px / var(--n));
-    background-position:calc(var(--i)/var(--n) * 16px) calc(var(--j)/var(--n) * 16px);
-    background-size:calc(64px / var(--n)) calc(16px / var(--n));
-    */
   }
+  
 </style>
