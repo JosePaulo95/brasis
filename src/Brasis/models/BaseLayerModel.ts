c872f29a722173e1a12eb5952a284824dcb2ed5e
Paulo <fernandes.josepaulo95@gmail.com>
2022-07-25 11:28:51 -0300

    add id static field

--- src/Brasis/models/BaseLayerModel.ts
+++ src/Brasis/models/BaseLayerModel.ts
@@ -1,8 +1,12 @@
 
 export default class BaseLayerModel{
     public value!: number;
+    public id: string;
+    public static next_id = 0;
 
     constructor(value = 0){
         this.value = value;
+        this.id = String(BaseLayerModel.next_id);
+        BaseLayerModel.next_id++;
     }
 }
