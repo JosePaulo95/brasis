4a8fc55a9197e257c4a580aeea6ea446bcc6ed8c
Paulo <fernandes.josepaulo95@gmail.com>
2022-07-26 22:15:07 -0300

    add files

--- src/Brasis/models/AudioModel.ts
+++ src/Brasis/models/AudioModel.ts
@@ -4,9 +4,11 @@ export default class AudioModel {
     files:any = {}
 
     constructor () {
-        this.files["select"] = new Audio(require('@/assets/audio/Sounds/Menu2/click1.ogg'))
+        this.files["select"] = new Audio(require('@/assets/audio/Sounds/Menu2/click4.ogg'))
+        this.files["on-square-to-move-selection"] = new Audio(require('@/assets/audio/Sounds/Menu2/click5.ogg'))
+        this.files["on-moving"] = new Audio(require('@/assets/audio/Sounds/Menu2/rollover2.ogg'))
         this.files["on-move-end"] = new Audio(require('@/assets/audio/Sounds/Menu2/rollover2.ogg'))
-        this.files["on-square-to-move-selection"] = new Audio(require('@/assets/audio/Sounds/Menu2/click3.ogg'))
+        this.files["cancel"] = new Audio(require('@/assets/audio/Sounds/Menu/Menu4.wav'))
     }
     find(index: string) {
         return this.files[index]
