fc437d37f4a12347ba2d62399ebe8ef3001db729
Paulo <fernandes.josepaulo95@gmail.com>
2022-07-30 18:45:22 -0300

    remove unused

--- src/Brasis/models/BoardModel.ts
+++ src/Brasis/models/BoardModel.ts
@@ -94,35 +94,6 @@ export default class BoardModel{
                     [0,0,0],
                 ].map(row => row.map(cell => new HUDLayerModel(cell)))        
                 break;
-        }
-        
-        
-    }
-    propagateSelection({ old_x, old_y, x, y}:{ old_x:number, old_y:number, x:number, y:number}): BoardModel {
-        //MOVE current hud select
-        //previous hud release
-        //current bg select
-        //previous actor release
-        //previous bg release
-        //SHOW_POSSIBLE_MOVES current actor select
-
-        const prev_selected = old_x!=undefined?this.hud_board[old_x][old_y].value:null
-        const prev_actor = old_x!=undefined?this.actors_board[old_x][old_y].value:null
-        const current_empty = this.actors_board[x][y].value==0
-
-        old_x!=undefined&&(this.hud_board[old_x][old_y].value = 0)
-
-        if(prev_actor && current_empty){
-            if(prev_selected){
-                this.actors_board[old_x][old_y].value = 0
-                this.actors_board[x][y].value = 1
-            }else{
-                this.hud_board[x][y].value = 1
-            }
-        }else{
-            this.hud_board[x][y].value = 1
-        }
-
-        return this
+        }   
     }
 }
