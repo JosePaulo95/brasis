71d287fbffea41713d96a882bd9af3b06ebf7387
Paulo <fernandes.josepaulo95@gmail.com>
2022-08-07 00:42:11 -0300

    update method playerwins

--- src/Brasis/models/LevelModel.ts
+++ src/Brasis/models/LevelModel.ts
@@ -5,16 +5,30 @@ import { PlayerModel } from "./PlayerModel";
 
 export class LevelModel {
     board: BoardModel;
-    
+    turn = 1;
+
     constructor (board: BoardModel, p1: PlayerModel, p2: PlayerModel) {
         this.board = board;
     }
     
     player1wins(): boolean {
-        const isPlayer2Actor = (cell: ActorLayerModel): boolean => {
-            return cell.value%2==0 && cell.value!=0
+        return this.playerwins(1)
+    }
+    
+    player2wins(): boolean {
+        return this.playerwins(2)
+    }
+    private playerwins(team_id: number): boolean {
+        const team = team_id==1?"ally":"enemy"
+
+        const isOpponent = (actor: ActorLayerModel): boolean => {
+            return actor.team != team
         }
         
-        return !this.board.actors_board.hasAny(isPlayer2Actor)
+        return !this.board.actors_board.hasAny(isOpponent)
+    }
+    
+    isPlayer1Turn(): boolean {
+        return this.turn==1;
     }
 }
