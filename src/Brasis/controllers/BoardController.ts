23872c554ea6aa6ab9fc569a1a5493ee5ca88152
Paulo <fernandes.josepaulo95@gmail.com>
2022-08-20 19:29:49 -0300

    removes path methods from controller

--- src/Brasis/controllers/BoardController.ts
+++ src/Brasis/controllers/BoardController.ts
@@ -123,51 +123,6 @@ export default class BoardController{
             this.model.action_square_board.at(house).value = 1
         }
     }
-    getNeighbors(p: Point, d=1, root=true): Array<Point> {
-        if(!this.model.bg_board.at(p) || (this.model.actors_board.at(p).value && !root)){
-            return []
-        }
-        
-        if(d==0){
-            return [p]
-        }
-        return [
-            this.getNeighbors(new Point(p.x,    p.y-1),    d-1, false),
-            this.getNeighbors(new Point(p.x,    p.y+1),    d-1, false),
-            this.getNeighbors(new Point(p.x-1,  p.y),      d-1, false),
-            this.getNeighbors(new Point(p.x+1,  p.y),      d-1, false)
-        ].filter(Boolean).flat(10).filter((elm, index, arr) => index == arr.findIndex(i=>i.x==elm.x&&i.y==elm.y))
-    }
-    calcShortestPath(a: Point, b: Point): Array<Point> {
-        const points:Array<Point> = [];
-        const d = this.getDistance(a,b)
-        if(d!=undefined){
-            let cur = a
-            points.push(cur)
-
-            for (let i = 0; i < d; i++) {
-                let neighbors = this.getNeighbors(cur, 1)
-                let neighbors_w_distance = neighbors.map(p => { return {d: this.getDistance(b, p), p}})
-                let sorted = neighbors_w_distance.sort((a,b)=>a.d-b.d)
-                let next = sorted[0]
-                cur = next.p
-                points.push(cur)
-            }
-        }
-        return points;
-    }
-    getDistance(a: Point, b: Point): number {
-        let d = 0, neighbors = [], found
-
-        do{
-            neighbors = this.getNeighbors(a, d)
-            found = neighbors.some(p => p.x==b.x && p.y==b.y)
-            d++
-        }while(d<10 && !found)
-
-
-        return found?d-1:Infinity
-    }
 }
 
 
